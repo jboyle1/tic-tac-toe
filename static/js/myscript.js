0d567d1b6234f345db0f2d4228543f59198a524f
Jed Boyle <jboyle1@pca.ac.uk>
2019-12-14 12:28:09 +0000

    Developed clearboard function and restart button.

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,1 +1,16 @@
-console.log("Connected!")
+// Restart game button
+var restart = document.querySelector('#b');
+// Grabs all the squares
+var squares = document.querySelectorAll('td')
+// Clear all the squares
+function clearboard() {
+    for (var i = 0; i < squares.length; i++ ) {
+        squares[i].textContent = '';
+    }
+}
+
+
+restart.addEventListener('click',clearboard);
+// Check the square marker
+
+// For loop to add event listeners to all the squares
